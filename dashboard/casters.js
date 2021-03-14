15bd9afd19d27175f07cdee7c89db662d22e796a
mph8344 <mph8344@g.rit.edu>
2021-03-13 19:19:54 -0500

    Added caster videos

--- dashboard/casters.js
+++ dashboard/casters.js
@@ -3,6 +3,11 @@ const rightCaster = document.getElementById('rightCaster')
 
 
 function update() {
-	const data = {leftCaster: leftCaster.value, rightCaster: rightCaster.value}
+	const data = {leftCaster: leftCaster.value, rightCaster: rightCaster.value, game: "valorant"}
+	nodecg.sendMessage('updateCasters', data)
+}
+
+function csgoUpdate() {
+	const data = {leftCaster: leftCaster.value, rightCaster: rightCaster.value, game: "cs"}
 	nodecg.sendMessage('updateCasters', data)
 }
