9f4b063f29e8ae5f3f2c00b332e8fffc277da6c8
mph8344 <mph8344@g.rit.edu>
2021-04-05 15:45:56 -0400

    Fixed issue with team names

--- dashboard/teams.js
+++ dashboard/teams.js
@@ -1,17 +1,11 @@
 const team1Input = document.getElementById('team1Name')
 const team2Input = document.getElementById('team2Name')
 
-const fileSelect1 = document.getElementById('team1Logo');
-const fileSelect2 = document.getElementById('team2Logo');
-
-
 function update() {
 
 	const data = {
 		team1Name: team1Input.value,
 		team2Name: team2Input.value,
-		team1Logo: fileSelect1.value,
-		team2Logo: fileSelect2.value
 	}
 	nodecg.sendMessage('CreateTeams', data);
 }
