be5a9a8e2c68fbaef9f8fbac40974723a6752c99
mph8344 <mph8344@g.rit.edu>
2021-06-08 16:07:00 -0400

    General code cleanup pt. 1

--- dashboard/casters.js
+++
@@ -1,9 +0,0 @@
-const leftCaster = document.getElementById('leftCaster')
-const rightCaster = document.getElementById('rightCaster')
-
-
-function update() {
-	const data = {leftCaster: leftCaster.value, rightCaster: rightCaster.value}
-	nodecg.sendMessage('updateCasters', data)
-}
-
