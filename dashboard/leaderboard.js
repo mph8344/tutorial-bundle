4fd5958f8593c1bda659502e0ad69543fd546d42
mph8344 <mph8344@g.rit.edu>
2021-06-09 16:41:57 -0400

    Added allstars stuff

--- dashboard/leaderboard.js
+++ dashboard/leaderboard.js
@@ -4,10 +4,47 @@ function generateTable() {
 	var i;
 	for (i = 0; i < 10; i++) {
 		//$('#main').append(`<div class="row"> ${i} </div>`);
-		$('#main').append(
-			`<input type="text" id="text${i + 1}" placeholder="Player ${
-				i + 1
-			}"> </div><br/>`
-		);
+
+		var textBox = document.createElement('input');
+		var intBox = document.createElement('input');
+		$(textBox).prop({
+			type: 'text',
+			id: `text${i + 1}`,
+			placeholder: `Player ${i + 1}`,
+		});
+
+		$(intBox).prop({
+			type: 'number',
+			id: `${i + 1}`,
+			prevplace: `${i + 1}`,
+			min: '1',
+			max: '10',
+			value: `${i + 1}`,
+		});
+
+		intBox.onchange = updateNumber;
+
+		$('#main').append(textBox, intBox, `<br/>`);
 	}
+}
+
+// function updateNumber() {
+// 	console.log(this.value);
+// }
+
+function updateNumber() {
+	var prevValue = this.prevplace; // 1
+	var curValue = this.value; // 2
+
+	var prevBox = document.getElementById(curValue);
+
+	if (prevBox) {
+		//console.log(prevText);
+
+		prevBox.value = prevBox.prevplace = prevBox.id = prevValue; // 1
+
+		this.prevplace = this.id = curValue; // 2
+	}
+
+	// console.log(box);
 }
